8fc95470fdd8ff166fd06b6069ad79a497db15b2
Daeric <daeric@hashicorp.com>
2022-11-30 10:59:22 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,7 @@ output "aws_tunnel1_public_ip" {
 output "aws_tunnel1_shared_key" {
   value       = aws_vpn_connection.site_to_site.tunnel1_preshared_key
   description = "AWS VPN Tunnel 1 Shared Key"
+  sensitive = true
 }
 
 output "aws_tunnel2_public_ip" {
@@ -18,5 +19,5 @@ output "aws_tunnel2_public_ip" {
 output "aws_tunnel2_shared_key" {
   value       = aws_vpn_connection.site_to_site.tunnel2_preshared_key
   description = "AWS VPN Tunnel 2 Shared Key"
+  sensitive = true
 }
-
